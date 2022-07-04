03bc84a1f49c9e2356eadb3fa7010fc3e6816680
King12376 <94012676+King12376@users.noreply.github.com>
2022-07-04 13:50:15 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+//Reversing numbers
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
+
+int main()
+{
+    //Reversing numbers
+   int a[N], i;
+
+     printf("Enter %d numbers:", N);
+       for(i=0; i<N; i++)
+         scanf("%d", &a[i] );
+
+         printf("In reverse order:");
+         for(i=N-1; i>=0; i--)
+            printf("%d", a[i]);
+
+            printf("\n");
+
+
+
+    return 0;
+}
